0a889ba620775eb8ebbb5ef9e7c6932a92451044
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2018-02-11 01:00:51 +0200

    Add filterTasks

--- tasklist/app.js
+++ tasklist/app.js
@@ -16,6 +16,8 @@ function loadEventListeners() {
     taskList.addEventListener('click', removeTask);
     // Clear task event
     clearBtn.addEventListener('click', clearTasks);
+    // Filter tasks event
+    filter.addEventListener('keyup', filterTasks);
 }
 
 // Add task
@@ -68,4 +70,20 @@ function clearTasks(event) {
     while (taskList.firstChild) {
         taskList.removeChild(taskList.firstChild);
     }
+}
+
+// Filter tasks
+function filterTasks(event) {
+    const text = event.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function(task) {
+        const item = task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
+
+    console.log(text);
 }
