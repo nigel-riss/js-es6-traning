db03f65171073793f732d1ce44208816699fa25e
nigel-riss <mr.kurenkov@gmail.com>
2018-02-15 15:50:23 +0200

    Finish OOP section

--- sandbox/app.js
+++ sandbox/app.js
@@ -1,8 +1,29 @@
-// function declaration
-function greet(firstName, lastName) {
-    
-    // console.log('Hello');
-    return 'Hello ' + firstName + ' ' + lastName;
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    greeting() {
+        return `Hello there ${this.firstName} ${this.lastName}`;
+    }
 }
 
-console.log(greet('John', 'Doe'));
+class Customer extends Person {
+    constructor(firstName, lastName, phone, membership) {
+        super(firstName, lastName);
+        this.phone = phone;
+        this.membership = membership;
+    }
+
+    static getMembershipCost() {
+        return 500;
+    }
+}
+
+const john = new Customer('John', 'Dow', '555-555-5555', 'Standard');
+
+console.log(john);
+console.log(john.greeting());
+
+console.log(Customer.getMembershipCost());
