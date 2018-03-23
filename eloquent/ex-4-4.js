316055752dfbb61dddc4aab33a1ff59268d86299
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2018-03-23 13:05:11 +0200

    Add solution for exercise 4 for Eloquent JS ch 4

--- eloquent/ex-4-4.js
+++ eloquent/ex-4-4.js
@@ -1,9 +1,39 @@
 // Deep comparson of objects
 // Compare if objects have same properties of same value
-function deepCompare(objA, objB) {
-    if (typeof objA == "object" && objA != null)
-    let keysA = objA.keys;
-    for (key in obj) {
+function deepEqual(valA, valB) {
+    // check if both are objects
+    if (typeof valA == 'object' 
+        && valA != null 
+        && typeof valB == 'object'
+        && valB != null) {
 
+        // check if both are the same object
+        if (valA == valB) return true;
+
+        // check if both have the same number of keys
+        let keysA = Object.keys(valA);
+        let keysB = Object.keys(valB);
+        if (keysA.length !== keysB.length) return false;
+    
+        // deep compare keys values
+        for (key in valA) {
+            if (key in valB) {
+                return deepEqual(valA[key], valB[key]);
+            } else {
+                return false;
+            }
+        }
+
+    // if one of values is not and object compare them
+    } else {
+        return valA === valB;
     }
 }
+
+let obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
