54e9df5e59ca38045749894a06436fa551575581
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2018-04-01 21:28:07 +0200

    Add code for ch 7 of Eloquent JS

--- eloquent/ch-7-1.js
+++ eloquent/ch-7-1.js
@@ -8,8 +8,6 @@ const roads = [
     "Marketplace-Town Hall",       "Shop-Town Hall"
 ];
 
-Array.call()
-
 /**
  * Build an map object that, for each node, stores an array
  * of connected nodes
@@ -72,7 +70,7 @@ function runRobot(state, robot, memory) {
     for (let turn = 0;; turn++) {
         if (state.parcels.length == 0) {
             console.log(`Done in ${turn} turns`);
-            break;
+            return turn;
         }
         let action = robot(state, memory);
         state = state.move(action.direction);
@@ -110,3 +108,47 @@ runRobot(VillageState.random(), randomRobot);
 // console.log(randomRobot(state));
 // console.log(randomRobot(state));
 // console.log(randomRobot(state));
+
+const mailRoute = [
+    "Alice's House", "Cabin", "Alice's House", "Bob's House",
+    "Town Hall", "Daria's House", "Ernie's House",
+    "Grete's House", "Shop", "Grete's House", "Farm",
+    "Marketplace", "Post Office"
+];
+
+function routeRobot(state, memory) {
+    if (memory.length == 0) {
+        memory = mailRoute;
+    }
+    return {direction: memory[0], memory: memory.slice(1)};
+}
+
+// runRobot(VillageState.random(), routeRobot, []);
+
+function findRoute(graph, from, to) {
+    let work = [{at: from, route: []}];
+    for (let i = 0; i < work.length; i++) {
+        let {at, route} = work[i];
+        for (let place of graph[at]) {
+            if (place == to) return route.concat(place);
+            if (!work.some(w => w.at == place)) {
+                work.push({at: place, route: route.concat(place)});
+            }
+        }
+    }
+}
+
+function goalOrientedRobot({place, parcels}, route) {
+    if (route.length == 0) {
+        let parcel = parcels[0];
+        if (parcel.place != place) {
+            route = findRoute(roadGraph, place, parcel.place);
+        } else {
+            route = findRoute(roadGraph, place, parcel.address);
+        }
+    }
+    return {direction: route[0], memory: route.slice(1)};
+}
+
+
+// Exercise 1. Measuring a robot
